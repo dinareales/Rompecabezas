378875482b408d3c19e3b3a5f8356425dcd1c01e
Dina <dinamar582@hotmail.com>
2019-02-28 01:02:04 -0500

    Faltan 2 pasos

--- js/juego.js
+++ js/juego.js
@@ -35,18 +35,29 @@ y utilice actualizarUltimoMovimiento para mostrarlo en pantalla */
 
 /* Esta función va a chequear si el Rompecabezas esta en la posicion ganadora. 
 Existen diferentes formas de hacer este chequeo a partir de la grilla. */
-function chequearSiGano(grilla) {
-  for (var i = 0; i < grilla.length; i++) {
-    for (var j = 0; j < grilla.length; j++) {
-      console.log(grilla[i][j]);
+function chequearSiGano() {
+  var grillaOrdenada = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+    ];
+
+    if (grillaOrdenada.length !== grilla.length) 
+      return false;
+
+    for (var i = 0; i < grilla.length; i++) {
+      for (var j = 0; j < grilla.length; j++) {
+        if(grillaOrdenada[i][j] !== grilla[i][j])
+          return false;
+      }
     }
-  }
-    //COMPLETAR
+    return true;
 }
 
 // Implementar alguna forma de mostrar un cartel que avise que ganaste el juego
 function mostrarCartelGanador() {
-    //COMPLETAR
+  if (chequearSiGano())
+    alert("Ganaste");
 }
 
 /* Función que intercambia dos posiciones en la grilla.
@@ -60,7 +71,9 @@ En vez de intercambiar esos valores vamos a terminar teniendo en ambas posicione
 Se te ocurre cómo solucionar esto con una variable temporal?
 */
 function intercambiarPosicionesGrilla(filaPos1, columnaPos1, filaPos2, columnaPos2) {
-    //COMPLETAR
+  var temporal = grilla[filaPos1][columnaPos1];
+  grilla[filaPos1][columnaPos1] = grilla[filaPos2][columnaPos2];
+  grilla[filaPos2][columnaPos2] = temporal;
 }
 
 // Actualiza la posición de la pieza vacía
